8092878f88d885c39feae0e40f791289b67214ef
Riccardo La Torre <ricc.latorre@gmail.com>
2023-01-07 21:08:06 +0000

    public web server

--- example1/main.tf
+++ example1/main.tf
@@ -5,7 +5,29 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress = [{
+    description = "Ingress from everywhere on 8080"
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    security_groups = []
+    self = false
+  }]
 }
