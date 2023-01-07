ee36dae295b4c9c4d632925a5c23eaa78a05492b
Riccardo La Torre <ricc.latorre@gmail.com>
2023-01-07 21:33:46 +0000

    i/o vars

--- example1/main.tf
+++ example1/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
@@ -9,7 +20,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   tags = {
     Name = "terraform-example"
@@ -22,9 +33,9 @@ resource "aws_security_group" "instance" {
   ingress = [{
     description = "Ingress from everywhere on 8080"
     cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 8080
+    from_port = var.server_port
     protocol = "tcp"
-    to_port = 8080
+    to_port = var.server_port
     ipv6_cidr_blocks = []
     prefix_list_ids = []
     security_groups = []
