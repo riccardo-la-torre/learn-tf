e8083763f83772a6909f785b97cf62de54dd0df1
Riccardo La Torre <ricc.latorre@gmail.com>
2023-02-05 22:48:59 +0000

    tf state

---
+++ state/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "riccardo-la-torre-tf-state"
+    lifecycle {
+        prevent_destroy = false
+    }
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+              sse_algorithm = "AES256"
+            }
+        }
+    }        
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "tf-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
+
+output "s3_bucket_arn" {
+    value = aws_s3_bucket.terraform_state.arn
+    description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+    value = aws_dynamodb_table.terraform_locks.name
+    description = "The name of the DynamoDB table"
+}
+
+# terraform {
+#   backend "s3" {
+#     bucket = "riccardo-la-torre-tf-state"
+#     key = "global/s3/terraform.tfstate"
+#     region = "us-east-2"
+#     dynamodb_table = "tf-locks"
+#     encrypt = true
+#   }
+# }
