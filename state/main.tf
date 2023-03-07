e267ce2d16bf585ef9ed64d4d62b3bd021aa7edd
Riccardo La Torre <ricc.latorre@gmail.com>
2023-03-07 22:14:53 +0000

    State and workspaces

--- state/main.tf
+++ state/main.tf
@@ -2,49 +2,17 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+terraform {
+  backend "s3" {
     bucket = "riccardo-la-torre-tf-state"
-    lifecycle {
-        prevent_destroy = false
-    }
-    versioning {
-        enabled = true
-    }
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-              sse_algorithm = "AES256"
-            }
-        }
-    }        
+    key = "workspaces-example/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "tf-locks"
+    encrypt = true
+  }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-    name = "tf-locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    attribute {
-      name = "LockID"
-      type = "S"
-    }
+resource "aws_instance" "example" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 }
-
-output "s3_bucket_arn" {
-    value = aws_s3_bucket.terraform_state.arn
-    description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-    value = aws_dynamodb_table.terraform_locks.name
-    description = "The name of the DynamoDB table"
-}
-
-# terraform {
-#   backend "s3" {
-#     bucket = "riccardo-la-torre-tf-state"
-#     key = "global/s3/terraform.tfstate"
-#     region = "us-east-2"
-#     dynamodb_table = "tf-locks"
-#     encrypt = true
-#   }
-# }
