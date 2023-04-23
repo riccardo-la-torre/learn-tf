a968fb53a5a48eb87319057e9f09bb61dab90e60
Riccardo La Torre <ricc.latorre@gmail.com>
2023-04-23 14:26:40 +0100

    template_file data source

--- file-layout/stage/services/webserver-cluster/main.tf
+++ file-layout/stage/services/webserver-cluster/main.tf
@@ -33,28 +33,21 @@ data "terraform_remote_state" "db" {
   }
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type = number
-  default = 8080
-}
+data "template_file" "user_data" {
+  template = file("user-data.sh")
 
-output "alb_dns_name" {
-  value = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
+  vars = {
+    server_port = var.server_port
+    db_address = data.terraform_remote_state.db.outputs.address
+    db_port = data.terraform_remote_state.db.outputs.port
+  }
 }
 
 resource "aws_launch_configuration" "example" {
   image_id = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   security_groups = [ aws_security_group.instance.id ]
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-              echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  user_data = data.template_file.user_data.rendered
   lifecycle {
     create_before_destroy = true
   }
